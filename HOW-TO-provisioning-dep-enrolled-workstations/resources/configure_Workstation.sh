cd26fb322542c2a374fbb8f5515156b67f6359c8
Jayson Kish <jkish2@users.noreply.github.com>
2018-07-31 16:47:51 -0400

    Update configure_Workstation.sh

--- HOW-TO-provisioning-dep-enrolled-workstations/resources/configure_Workstation.sh
+++ HOW-TO-provisioning-dep-enrolled-workstations/resources/configure_Workstation.sh
@@ -6,34 +6,50 @@
 # password for the standard local user account that you're creating. For more information on using script parameters,
 # please see https://www.jamf.com/jamf-nation/articles/146/script-parameters.
 
+
+#########################
+# VARIABLE DECLARATIONS
+#########################
+
 adminUser="$4"
 adminPassword="$5"
 userName="$6"
 userPassword="$7"
+firmwarePassword="$8"
+
+
+#########################
+# FUNCTION DEFINITIONS
+#########################
 
 # Create a standard local user account.
 createLocalUserAccounts() {
     if [ "$(dscl . list /Users | grep "$userName")" == "" ]; then
-        sysadminctl -adminUser $adminUser -adminPassword $adminPassword -addUser $userName -password $userPassword -picture /Library/User\ Pictures/Animals/Parrot.tif
+        /usr/sbin/sysadminctl -adminUser $adminUser -adminPassword $adminPassword -addUser $userName -password $userPassword -picture /Library/User\ Pictures/Animals/Parrot.tif
     fi
 }
 
 # Set the computer name to something uniquely generated (Example: DEPC03FF3GFGFBB).
 enableComputerName() {
     computerName="DEP`system_profiler SPHardwareDataType | awk '/Serial/ {print $4}'`"
-    scutil --set ComputerName $computerName
-    scutil --set HostName $computerName
+    /usr/sbin/scutil --set ComputerName $computerName
+    /usr/sbin/scutil --set HostName $computerName
 }
 
 # Enable File Sharing.
 enableFileSharing() {
-    launchctl load -w /System/Library/LaunchDaemons/com.apple.smbd.plist
+    /bin/launchctl load -w /System/Library/LaunchDaemons/com.apple.smbd.plist
+}
+
+# Enable Firmware Password.
+enableFirmwarePassword() {
+    /usr/bin/expect -c "spawn /usr/sbin/firmwarepasswd -setpasswd ; expect ":" ; send "$firmwarePassword\\n" ; expect ":" ; send "$firmwarePassword\\n" ; interact"
 }
 
 # Enable Remote Login, ($adminUser only).
 enableRemoteLogin() {
-    systemsetup -setremotelogin on
-    dseditgroup -o edit -a $adminUser -t user com.apple.access_ssh
+    /usr/sbin/systemsetup -setremotelogin on
+    /usr/sbin/dseditgroup -o edit -a $adminUser -t user com.apple.access_ssh
 }
 
 # Enable Remote Management, ($userName only).
@@ -44,35 +60,39 @@ enableRemoteManagement() {
 
 # Enable all users for printer administration.
 enablePrinterAdministration() {
-    security authorizationdb write system.print.admin allow
+    /usr/bin/security authorizationdb write system.print.admin allow
 }
 
 # Enable Location Services.
 enableLocationServices() {
-    defaults write /var/db/locationd/Library/Preferences/ByHost/com.apple.locationd LocationServicesEnabled -int 1
-    chown -R _locationd:_locationd /var/db/locationd
+    /usr/bin/defaults write /var/db/locationd/Library/Preferences/ByHost/com.apple.locationd LocationServicesEnabled -int 1
+    /usr/sbin/chown -R _locationd:_locationd /var/db/locationd
 }
 
 # Enable Location-Based Time Zone.
 enableLocationBasedTimeZone() {
-    defaults write /Library/Preferences/com.apple.timezone.auto Active -bool true
+    /usr/bin/defaults write /Library/Preferences/com.apple.timezone.auto Active -bool true
 }
 
 # Disable Computer Sleep.
 disableComputerSleep() {
-    pmset -a sleep 0
+    /usr/bin/pmset -a sleep 0
 }
 
 # Flush the Policy History for the computer on JAMF Pro.
 flushPolicyHistory() {
-    jamf flushPolicyHistory
+    /usr/local/bin/jamf  flushPolicyHistory
 }
 
-#
+
+#########################
+# SCRIPT BODY
+#########################
 
 createLocalUserAccounts
 enableComputerName
 enableFileSharing
+enableFirmwarePassword
 enableRemoteLogin
 enableRemoteManagement
 enablePrinterAdministration
